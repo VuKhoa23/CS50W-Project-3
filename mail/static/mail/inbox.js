9c1a47202fa9745f4a9cf00dcc45495fb246e9c0
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-07-26 21:14:56 +0700

    Finish several features
    View email, Un/Archive email, display different background when the mail is read

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -47,6 +47,8 @@ async function send(event) {
 function compose_email() {
   // Show compose view and hide other views
   document.querySelector("#emails-view").style.display = "none";
+  document.querySelector("#email-content").style.display = "none";
+
   document.querySelector("#compose-view").style.display = "block";
 
   // Clear out composition fields
@@ -55,13 +57,104 @@ function compose_email() {
   document.querySelector("#compose-body").value = "";
 }
 
-function load_mailbox(mailbox) {
+async function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector("#emails-view").style.display = "block";
   document.querySelector("#compose-view").style.display = "none";
+  document.querySelector("#email-content").style.display = "none";
 
   // Show the mailbox name
   document.querySelector("#emails-view").innerHTML = `<h3>${
     mailbox.charAt(0).toUpperCase() + mailbox.slice(1)
   }</h3>`;
+  // TOOD
+  emails = [];
+  try {
+    console.log(mailbox);
+    response = await fetch(`/emails/${mailbox}`);
+    emails = await response.json();
+    console.log(emails);
+  } catch (e) {
+    console.log(e);
+  }
+  emails.forEach((email) => {
+    const element = document.createElement("div");
+    element.classList.add("email-box");
+    element.style.margin = "10px";
+    element.style.border = "1px solid darkgray";
+    element.style.borderRadius = "5px";
+    element.style.padding = "5px";
+    element.style.cursor = "pointer";
+    element.innerHTML = `
+    <h6>Sender: ${email.sender}</h6>
+    <h6>Recipients: ${email.recipients.toString()} </h6>
+    <span><strong>Subject:</strong> ${email.subject} - <strong>Time: </strong>${
+      email.timestamp
+    }</span>
+    `;
+    if (email.read === true) {
+      element.style.backgroundColor = "white";
+    } else {
+      element.style.backgroundColor = "lightgray";
+    }
+    document.querySelector("#emails-view").appendChild(element);
+
+    element.addEventListener("click", () => {
+      view_email(email.id, mailbox);
+    });
+  });
+}
+
+async function view_email(id, mailbox) {
+  try {
+    response = await fetch(`/emails/${id}`);
+    email = await response.json();
+
+    if (email.read === false) {
+      fetch(`/emails/${email.id}`, {
+        method: "PUT",
+        body: JSON.stringify({
+          read: true,
+        }),
+      });
+    }
+
+    document.querySelector("#emails-view").style.display = "none";
+    document.querySelector("#compose-view").style.display = "none";
+    document.querySelector("#email-content").style.display = "block";
+
+    element = document.querySelector("#email-content");
+    element.innerHTML = `
+    <h6>Sender: ${email.sender}</h6>
+    <h6>Recipients: ${email.recipients.toString()} </h6>
+    <span><strong>Subject:</strong> ${email.subject} - <strong>Time: </strong>${
+      email.timestamp
+    }</span>
+    <hr/>
+    <p>${email.body}</p>
+    `;
+
+    if (mailbox !== "sent") {
+      archiveBtn = document.createElement("button");
+      if (!email.archived) {
+        archiveBtn.innerHTML = "Archive";
+        archiveBtn.className = "btn btn-danger";
+      } else {
+        archiveBtn.innerHTML = "Un-archive";
+        archiveBtn.className = "btn btn-success";
+      }
+      archiveBtn.addEventListener("click", async function () {
+        await fetch(`/emails/${email.id}`, {
+          method: "PUT",
+          body: JSON.stringify({
+            archived: email.archived ? false : true,
+          }),
+        });
+        email.archived ? load_mailbox("inbox") : load_mailbox("archive");
+      });
+      element.appendChild(archiveBtn);
+    }
+  } catch (e) {
+    console.log(e);
+  }
 }
